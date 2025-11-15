c069d5c1bc7814cc011760ceeb60766709f23337
Mohil-Wankar <work.mohilwankar@gmail.com>
2025-11-15 15:31:17 +0530

    Added Agents Data Table to Dashboard

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -3,16 +3,26 @@
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client"; 
+import { EmptyState } from "@/components/empty-state";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
+
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
 
 export const AgentView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
     return (
-        <div>
-            {JSON.stringify(data, null, 2)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data={data} columns={columns}/>
+            {data.length === 0 && (
+                <EmptyState
+                    title="Create your first Agent"
+                    description="Create an AI Agent to get started. Each agent can be customized to fit your needs, and will follow your instructions during meetings to interact with participants during the call."             
+                />
+            )}
         </div>
     );
 };
